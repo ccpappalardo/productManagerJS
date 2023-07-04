e93dd053a8f1363a8e3b9f8a4c89eec7be018970
ccpappalardo <ccpappalardo@gmail.com>
2023-07-03 23:36:44 -0300

    modificacion v3 login de usuario

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -36,18 +36,30 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   console.log(email, password)
+  let user
+  console.log(email!='adminCoder@coder.com')
+    if(email!='adminCoder@coder.com')
+    { user = await managerUsers.getUserByEmailPass(email,password); 
+      if (!user) return res.redirect('/api/login')
+      req.session.user = {
+        name: user.first_name + user.last_name,
+        email: user.email,
+        age: user.age,
+        rol: "user"
+      };
+    }else{
+
+        req.session.user = {
+        name: "Admin Coder",
+        email: "adminCoder@coder.com",
+        password: "adminCod3r123",
+        rol: "admin"
+      };
+    }
+    res.send({ status: "success", message: req.session.user });
   
-  const user = await managerUsers.getUserByEmailPass(email,password);
   // userModel.findOne({ email: email, password: password });
-  console.log(user)
-  
-  if (!user) return res.redirect('/api/login')
-  req.session.user = {
-    name: user.first_name + user.last_name,
-    email: user.email,
-    age: user.age,
-  };
-  res.send({ status: "success", message: req.session.user });
+   
   
 });
 
@@ -56,8 +68,8 @@ router.get('/logout', (req, res) => {
     if (err) {
       return res.json({ status: 'Logout ERROR', body: err })
     }
-    res.send('Logout ok!')
-    res.redirect('/api/login')
+    return res.send('Logout ok!')
+    //res.redirect('/api/login')
   })
  })
  
